a134a244184bb7cba4c423e7f95c8a0bcd51d980
masud <masudranaacb@gmail.com>
2024-06-26 17:20:03 +0600

    links and useRoute using

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,14 +1,39 @@
+"use client"
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const Navber = () => {
+    let activePath=usePathname()
+    const router=useRouter()
+    const navlinks=[
+        {
+            title:"Home",
+            path:"/"
+        },
+        {
+            title:"About",
+            path:"/about"
+        },
+        {
+            title:"Contact",
+            path:"/contact"
+        },
+        {
+            title:"Services",
+            path:"/services"
+        },
+      
+    ]
+    const loginHandler=()=>{
+        router.push("/about")
+    }
     return (
-        <nav className='flex justify-between'>
+        <nav className='flex justify-between px-2 py-1'>
             <h2 className='text-3xl'>Nex.js</h2>
             <ul className='flex gap-4'>
-                <li className=''>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Services</li>
+               {navlinks?.map(navlink=><Link key={navlink.path} href={navlink.path} className={`${navlink.path==activePath && "bg-green-400"}`}>{navlink.title}</Link>)}
+               <button onClick={loginHandler} className='bg-gray-900 px-2'>Login</button>
             </ul>
         </nav>
     );
